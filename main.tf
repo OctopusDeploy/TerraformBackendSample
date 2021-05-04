47b8c370aa62e97330bd78b630abf1e9d4352024
Egor Pavlikhin <egor.pavlikhin@gmail.com>
2021-05-04 10:23:05 +1000

    Add AWS access key

--- main.tf
+++ main.tf
@@ -7,6 +7,16 @@ terraform {
   }
 }
 
+variable "aws_access_key" {
+  type = string
+}
+
+variable "aws_secret_key" {
+  type = string
+}
+
 provider "aws" {
   region     = "us-east-1"
+  access_key = aws_access_key
+  secret_key = aws_secret_key
 }
--- terraform.auto.tfvars
+++ terraform.auto.tfvars
@@ -1,1 +1,3 @@
 bucket_name = "#{BucketName}"
+aws_access_key = "#{AWS Account.AccessKey}"
+aws_secret_key = "#{AWS Account.SecretKey}"
